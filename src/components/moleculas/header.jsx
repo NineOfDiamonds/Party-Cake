0a372520f319657d6354c90464a41dc6f2b10a0f
NineOfDiamonds <fedorkravcenko77@gmail.com>
2022-03-09 20:47:28 +0300

    Added antd

---
+++ src/components/moleculas/header.jsx
@@ -0,0 +1,19 @@
+import {Button, PageHeader} from 'antd';
+export const Header = () => {
+    return (
+        <>
+            <PageHeader
+                className={'PageHeader'}
+                title={"Party Cake"}
+                subTitle={"Let the sweet begin!"}
+                extra={[
+                    <Button key="2">No</Button>,
+                    <Button key="1" type="primary">да</Button>,
+                ]}
+            />
+
+        </>
+
+    )
+
+}
