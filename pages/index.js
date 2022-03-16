2adccba26a11589a2b795e6d7c8910896de34727
NineOfDiamonds <fedorkravcenko77@gmail.com>
2022-03-16 18:08:39 +0300

    Added Seo

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,15 @@
 import {Header} from "../src/components/moleculas/Header";
 import {Content, Footer} from "antd/lib/layout/layout";
 import Title from "antd/lib/typography/Title"
+import Head from 'next/head'
 
 export default function Home() {
     return (
         <>
+            <Head>
+                <title>Example</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <Header/>
             <Content style={{margin:'60px 0 40px', textAlign: 'center'}}>
                 <Title type={'success'}>Найдите крутой торт или создайте его сами!</Title>
