a749616b8b6bc18c454cc59d12d4e7aab1568882
NineOfDiamonds <fedorkravcenko77@gmail.com>
2022-04-13 20:00:21 +0300

    Completed third section

--- src/components/organisms/whatToDo/index.jsx
+++ src/components/organisms/whatToDo/index.jsx
@@ -6,30 +6,36 @@ export const SectionDo = () => {
         <>
             <Content style={{ padding: '0 50px'}}>
                 <div style={{height:'100vh', padding: '24px'}} className="site-layout-content">
-                    <h2 className={'section-title'}>Четыре простых шага</h2>
-                    <Row>
-                        <Col span={12}>
-                            <div className={'step-body'}>
-                                <div className={'step-number'}><p style={{margin:'0', fontSize:'17px'}}>1</p></div>
-                                <p style={{fontSize:'20px'}}>Выберите одну из 10 начинок</p>
-                                <a href="/">Start</a>
-                            </div>
-                            <div className={'step-body'}>
-                                <div className={'step-number'}><p style={{margin:'0', fontSize:'17px'}}>3</p></div>
-                                <p style={{fontSize:'20px'}}>Наблюдайте за статусом и сроками</p>
-                            </div>
-                        </Col>
-                        <Col span={12}>
-                            <div className={'step-body'}>
-                                <div className={'step-number'}><p style={{margin:'0', fontSize:'17px'}}>2</p></div>
-                                <p style={{fontSize:'20px'}}>Загрузите картинку, или выберите готовый дизайн</p>
-                            </div>
-                            <div className={'step-body'}>
-                                <div className={'step-number'}><p style={{margin:'0', fontSize:'17px'}}>4</p></div>
-                                <p style={{fontSize:'20px'}}>Удивите близких классным подарком</p>
-                            </div>
-                        </Col>
+                    <h2 className={'section-title'}>Четыре быстрых шага</h2>
 
+                    <Row style={{width:'100%', display: 'flex',justifyContent:'center', flexDirection:'row', marginTop:'60px'}}>
+
+                        <div className={'step-body'} style={{fontSize:'24px', maxWidth:'25%'}}>
+                            <div className={'step-number'}><p style={{margin:'0', fontSize:'25px'}}>1</p></div>
+                            <p style={{margin:'0'}}>Выберите одну из 10 начинок</p>
+                            <a href="/" style={{color: '#ae7875', textDecoration:'underline'}}>Начать</a>
+                        </div>
+                        
+                        
+                        <div className={'step-body'} style={{fontSize:'24px', maxWidth:'25%'}}>
+                            <div className={'step-number'}><p style={{margin:'0', fontSize:'25px'}}>2</p></div>
+                            <p>Загрузите картинку, или выберите готовый дизайн</p>
+                        </div>
+                        
+                    </Row>
+                    <Row style={{width:'100%', display: 'flex', justifyContent:'center', flexDirection:'row'}}>
+                        
+                        <div className={'step-body'} style={{fontSize:'24px', maxWidth:'25%'}}>
+                            <div className={'step-number'}><p style={{margin:'0', fontSize:'25px'}}>3</p></div>
+                            <p>Наблюдайте за статусом и сроками</p>
+                        </div>
+                    
+                        
+                        <div className={'step-body'} style={{fontSize:'24px', maxWidth:'25%'}}>
+                            <div className={'step-number'}><p style={{margin:'0', fontSize:'25px'}}>4</p></div>
+                            <p style={{color:'#9d4045'}}>Удивите близких классным подарком</p>
+                        </div>
+                        
 
                     </Row>
                 </div>
